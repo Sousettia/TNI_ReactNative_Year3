5cc1c94f8af61ad5c1dc8bac243cfc6de597a8c5
Sousettia <aceofkuro3554@gmail.com>
2024-08-10 09:58:57 +0700

    Practice4_TextInput Component [August, 10,2024]

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,13 +1,61 @@
-import { View, Text, TextInput, Button } from "react-native";
-import React from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import React, { useState } from "react";
 import { stylesLogin } from "../styles/styles";
 
 const Login = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const validateEmail = (email:string) : boolean => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+  }
+  const handleLogin = () => {
+    let errorMessage = "";
+    if (!name) {
+      errorMessage += "Please Enter Name\n"; /*Alert.alert("Alert", "Please Enter Name", [{ text: "OK" }]); return;*/
+    }
+    if (!email) {
+      errorMessage += "Please Enter Email\n";
+    }
+    else if (!validateEmail(email)){
+      errorMessage += "Invalid Email Format\n"
+    }
+    if (!password) {
+      errorMessage += "Please Enter Password";
+    } 
+    else if (password.length < 6){
+      errorMessage += "Password must be at least 6 characters long";
+    }
+    if (errorMessage) {
+      Alert.alert("Error", errorMessage.trim(), [{ text: "OK" }]);
+      return;
+    }
+    Alert.alert("Alert","Success",[{text:"OK"}])
+  };
+
   return (
     <View style={stylesLogin.container}>
-      <TextInput style={stylesLogin.input} placeholder="Enter Name" />
-      <TextInput style={stylesLogin.input} placeholder="Enter Email" />
-      <Button title="SUBMIT"></Button>
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="SUBMIT" onPress={handleLogin}></Button>
     </View>
   );
 };
