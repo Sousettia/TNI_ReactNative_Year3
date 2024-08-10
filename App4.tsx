434676c8df0bf45b1c94d262a8c5eb59a08d0c3a
Sousettia <aceofkuro3554@gmail.com>
2024-08-10 11:21:54 +0700

    save login to app4

---
+++ App4.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View , TextInput} from "react-native";
+import React from "react";
+import ProfileScreen from "./components/ProfileScreen";
+
+const App = () => {
+  return (
+    <View>
+      <ProfileScreen />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
