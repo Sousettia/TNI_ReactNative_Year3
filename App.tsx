2010e3ec467ef69c870ce5991a7ea321221b0592
Sousettia <aceofkuro3554@gmail.com>
2024-08-31 11:57:50 +0700

    Lecture8 [August, 31,2024]

--- App.tsx
+++ App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import HomeScreen from './screens/HomeScreen';
-import AboutScreen from './screens/AboutScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import CreatePostScreen from './screens/CreatePostScreen';
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import CreatePostScreen from "./screens/CreatePostScreen";
 
 const App = (): React.JSX.Element => {
-
   const HomeStack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <HomeStack.Navigator initialRouteName='Home'>
-        <HomeStack.Screen name='Home' component={HomeScreen} />
-        <HomeStack.Screen name='About' component={AboutScreen} />
-        <HomeStack.Screen name='Post' component={CreatePostScreen} />
+      <HomeStack.Navigator initialRouteName="Home"
+        screenOptions={{//Global
+          headerStyle:{backgroundColor:'#f1f'},
+          headerTintColor:'white',
+          headerTitleStyle:{fontWeight:'bold'},
+        }}
+        >
+        <HomeStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "หน้าหลัก",
+          }}
+        />
+        <HomeStack.Screen
+          name="About"
+          component={AboutScreen}
+          /*           options={{
+          title:'เกี่ยวกับเรา',
+          headerStyle:{backgroundColor:'#f1f'},
+          headerTintColor:'white',
+          headerTitleStyle:{fontWeight:'bold'},
+          headerTitleAlign:'center'
+         }} */
+        />
+        <HomeStack.Screen name="Post" component={CreatePostScreen} />
       </HomeStack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 import React from "react";
+import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const HomeScreen = ({ navigation,route }: any): React.JSX.Element => {
+const HomeScreen = ({ navigation, route }: any): React.JSX.Element => {
   const gotoAbout = () => {
     navigation.navigate("About", {
       companyName: "Thai-Nichi Institute of Technology",
@@ -11,11 +12,15 @@ const HomeScreen = ({ navigation,route }: any): React.JSX.Element => {
 
   return (
     <View style={styles.container}>
+      <MaterialIcon name="home" size={50} color={"pink"} />
       <Text style={styles.header}>HomeScreen</Text>
       <Button title="About us" onPress={gotoAbout}></Button>
 
       <View style={styles.postContainer}>
-        <Button title="Create Post" onPress={()=>navigation.navigate("Post")}></Button>
+        <Button
+          title="Create Post"
+          onPress={() => navigation.navigate("Post")}
+        ></Button>
         <Text style={styles.postText}>Post:</Text>
         <Text style={styles.postContent}>{route.params?.post}</Text>
       </View>
