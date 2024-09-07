caa30496625fa91fd958e55d4a3e2db35c15caca
Sousettia <aceofkuro3554@gmail.com>
2024-09-07 11:27:19 +0700

    Lecture9_Navigation [September, 07,2024]

--- App.tsx
+++ App.tsx
@@ -1,43 +1,87 @@
+// Only import react-native-gesture-handler on native platforms
+import "react-native-gesture-handler";
+
 import React from "react";
+
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
+import MenuScreen from "./screens/MenuScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import CreatePostScreen from "./screens/CreatePostScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { HeaderButtonsProvider } from "react-navigation-header-buttons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-const App = (): React.JSX.Element => {
-  const HomeStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const ProductStack = createNativeStackNavigator();
 
+function HomeStackScreen() {
   return (
-    <NavigationContainer>
-      <HomeStack.Navigator initialRouteName="Home"
-        screenOptions={{//Global
-          headerStyle:{backgroundColor:'#f1f'},
-          headerTintColor:'white',
-          headerTitleStyle:{fontWeight:'bold'},
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        //Global
+        // headerStyle:{backgroundColor:'#f1f'},
+        // headerTintColor:'white',
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        // options={{
+        //   title: "หน้าหลัก",
+        // }}
+      />
+      <HomeStack.Screen
+        name="About"
+        component={AboutScreen}
+        options={{
+          title: "เกี่ยวกับเรา",
+          headerStyle: { backgroundColor: "#f1f" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerTitleAlign: "center",
         }}
-        >
-        <HomeStack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "หน้าหลัก",
-          }}
-        />
-        <HomeStack.Screen
-          name="About"
-          component={AboutScreen}
-          /*           options={{
-          title:'เกี่ยวกับเรา',
-          headerStyle:{backgroundColor:'#f1f'},
-          headerTintColor:'white',
-          headerTitleStyle:{fontWeight:'bold'},
-          headerTitleAlign:'center'
-         }} */
-        />
-        <HomeStack.Screen name="Post" component={CreatePostScreen} />
-      </HomeStack.Navigator>
-    </NavigationContainer>
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+function ProductStackScreen() {
+  return (
+    <ProductStack.Navigator
+      initialRouteName="Products"
+      screenOptions={{
+        //Global
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <ProductStack.Screen 
+        name="Products"
+        component={ProductScreen}
+      />
+    </ProductStack.Navigator>
+  );
+}
+const App = (): React.JSX.Element => {
+  return (
+    <SafeAreaProvider>
+      <HeaderButtonsProvider stackType="native">
+        <NavigationContainer>
+          <Drawer.Navigator
+            screenOptions={{ headerShown: false }}
+            drawerContent={(props) => <MenuScreen {...props} />}
+          >
+            <Drawer.Screen name="HomeStack" component={HomeStackScreen} />
+            <Drawer.Screen name="ProductStack" component={ProductStackScreen}/>
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </HeaderButtonsProvider>
+    </SafeAreaProvider>
   );
 };
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,45 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, Button, StyleSheet, Alert } from "react-native";
+import React, { useLayoutEffect } from "react";
 import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
+import AppLogo from "../components/AppLogo";
+import {
+  Item,
+  HeaderButtons,
+  HeaderButton,
+} from "react-navigation-header-buttons";
 
-const HomeScreen = ({ navigation, route }: any): React.JSX.Element => {
+const MaterialHeaderButton = (props: any) => (
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  <HeaderButton IconComponent={MaterialIcon} iconSize={23} {...props} />
+);
+
+const HomeScreen = (): React.JSX.Element => {
+  const navigation = useNavigation<any>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "หน้าหลัก",
+      headerTitle: () => <AppLogo />,
+      headerTitleAlign: "center",
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item title="menu" iconName="menu" onPress={() => {
+            navigation.openDrawer();
+          }}/>
+        </HeaderButtons>
+      ),
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item title="logout" iconName="logout" onPress={() => {
+            Alert.alert("Logout","Close Menu");
+          }}/>
+        </HeaderButtons>
+      )
+    });
+  }, [navigation]);
+
   const gotoAbout = () => {
     navigation.navigate("About", {
       companyName: "Thai-Nichi Institute of Technology",
@@ -15,15 +52,6 @@ const HomeScreen = ({ navigation, route }: any): React.JSX.Element => {
       <MaterialIcon name="home" size={50} color={"pink"} />
       <Text style={styles.header}>HomeScreen</Text>
       <Button title="About us" onPress={gotoAbout}></Button>
-
-      <View style={styles.postContainer}>
-        <Button
-          title="Create Post"
-          onPress={() => navigation.navigate("Post")}
-        ></Button>
-        <Text style={styles.postText}>Post:</Text>
-        <Text style={styles.postContent}>{route.params?.post}</Text>
-      </View>
     </View>
   );
 };
